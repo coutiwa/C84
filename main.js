41b4cbdc73120dffef1296001311c985ada8243e
coutiwa <100156756+coutiwa@users.noreply.github.com>
2023-06-19 16:13:11 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,55 @@
+canvas = document.getElementById('myCanvas');
+
+img_width = 300;
+img_height = 100;
+
+var img_image;
+
+img_x = 100;
+img_y = 100;
+
+function add() {
+	img_imgTag = new Image(); //definindo uma variável com uma nova imagem
+	img_imgTag.onload = uploadimg; // ajustando uma função, ao carregar essa variável
+	img_imgTag.src = img_image;   // carregar uma imagem
+}
+
+function uploadimg() {
+
+	ctx.drawImage(img_imgTag, img_x, img_y, img_width, img_height);
+}
+window.addEventListener("keydown", my + keydown);
+function my_keydown(e) {
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+
+	if ((keyPressed >= 97 && keyPressed <= 122) || (keyPressed >= 65 && keyPressed <= 90)) {
+		aplhabetkey();
+		document.getElementById("d1").innerHTML = "Você pressionou uma tecla alfabeto";
+		console.log("alphabet key");
+	}
+
+	else {
+		otherkey();
+		document.getElementById("d1").innerHTML = "You pressed symbol or other key";
+	}
+}
+
+function aplhabetkey() {
+	img_image = "alfabeto.png";
+	add();
+
+}
+function numberkey() {
+
+}
+function arrowkey() {
+}
+function specialkey() {
+
+}
+function otherkey() {
+	img_image = "otherkey.png";
+	add();
+}
+
